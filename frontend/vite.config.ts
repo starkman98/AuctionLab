f485f7d9c1509190b35e6cc1883a49ff3a8bbf64
starkman98 <ant.stark@hotmail.com>
2026-05-14 15:56:18 +0200

    Configure vite and ts config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+
+  resolve: { alias: { "@": "/src" } },
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://localhost:7243",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});

3a26a125ce3fd32b62aba1ea78b35aa77fece7bf
starkman98 <ant.stark@hotmail.com>
2026-05-15 01:14:46 +0200

    Add LoginPage with simple LoginForm, BrowserRouter, And Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
+import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import ProtectedRoute from "./router/ProtectedRoute";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/auctions"
+        element={
+          <ProtectedRoute>
+            <p>Auctions page coming soon</p>
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/auctions" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,3 +1,66 @@
-const LoginPage = () => {};
+import { loginApi } from "@/api/authApi";
+import { getMe } from "@/api/userApi";
+import { useAuth } from "@/hooks/useAuth";
+import type { LoginRequest } from "@/types/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+const LoginPage = () => {
+  const { setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState<LoginRequest>({
+    userName: "",
+    password: "",
+  });
+  const [error, setError] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+  const handleSubmit = async () => {
+    setError(null);
+    setIsSubmitting(true);
+
+    try {
+      await loginApi(form);
+      const user = await getMe();
+      setUser(user);
+      navigate("/auctions");
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "Login failed");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        void handleSubmit();
+      }}
+    >
+      <input
+        name="userName"
+        value={form.userName}
+        onChange={handleChange}
+        placeholder="Username"
+        type="text"
+      />
+      <input
+        name="password"
+        value={form.password}
+        onChange={handleChange}
+        placeholder="Password"
+        type="password"
+      />
+      {error && <p>{error}</p>}
+      <button type="submit" disabled={isSubmitting}>
+        {isSubmitting ? "Loggar in..." : "Logga in"}
+      </button>
+    </form>
+  );
+};
 
 export default LoginPage;

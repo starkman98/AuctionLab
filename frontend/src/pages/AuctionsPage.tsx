de779fd1061ba6f0f2e1930cc366d413609e0f1d
starkman98 <ant.stark@hotmail.com>
2026-05-27 15:04:19 +0200

    Implemented search functionality to auctions page.

--- frontend/src/pages/AuctionsPage.tsx
+++ frontend/src/pages/AuctionsPage.tsx
@@ -1,20 +1,30 @@
 import { getAuctions } from "@/api/auctionApi";
 import AuctionListCard from "@/components/auction/AuctionListCard";
+import { useDebounce } from "@/hooks/useDebounce";
 import type { AuctionSummaryResponse } from "@/types/auction";
 import { useEffect, useState } from "react";
 
 const AuctionsPage = () => {
   const [auctions, setAuctions] = useState<AuctionSummaryResponse[]>([]);
 
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 300);
+
   useEffect(() => {
     const fetchAuctions = async () => {
-      setAuctions(await getAuctions());
+      setAuctions(await getAuctions(debouncedSearch));
     };
     fetchAuctions();
-  }, []);
+  }, [debouncedSearch]);
   return (
     <section className="text-lg">
       <h1>Auctions</h1>
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search auctions..."
+        className="border px-3"
+      />
       <AuctionListCard auctions={auctions} />
     </section>
   );
